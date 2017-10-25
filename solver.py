7f9e327885454fc2230b5811f40f0e8269ad73c7
pr3mar <markoprelevik@gmail.com>
2017-10-26 00:28:37 +0200

    Simplify done

--- solver.py
+++ solver.py
@@ -16,39 +16,35 @@ def handle_input(input_file_name):
         for line in file:
             line = line.strip().split(' ')
             if line[0] == 'c':
-                if len(line) > 1:
-                    print('A comment: %', ' '.join(line[1:]))
                 continue
             if line[0] == 'p':
-                input_type = line[1]
                 num_vars = int(line[2])
                 num_clauses = int(line[3])
                 continue
-            print(line[:-1])
             for var in line[:-1]:
                 var = abs(int(var))
-                if var in used_vars:
-                    print('Used:', used_vars[var],end='; ')
-                else:
+                if not(var in used_vars):
                     used_vars[var] = boolean.Variable(var)
-                    print('New: ', used_vars[var],end='; ')
-            print()
-            formula.append(boolean.Or([[used_vars[int(x)] if int(x) > 0 else boolean.Not(used_vars[abs(int(x))]) for x in line[:-1]], len(line[:-1])]))
+            formula.append(
+                boolean.Or(
+                    [
+                        [used_vars[int(x)] 
+                        if int(x) > 0 
+                        else boolean.Not(used_vars[abs(int(x))]) 
+                        for x in line[:-1]], len(line[:-1])
+                    ]
+                )
+            )
         formula = boolean.And([formula, len(formula)])
-        print(formula)
-    return (input_type, num_vars, num_clauses, formula)
+        return (num_vars, num_clauses, formula)
 
 if __name__ == '__main__':
-    if len(sys.argv) == 3:
+    if len(sys.argv) >= 3:
         input_file_name = sys.argv[1]
         output_file_name = sys.argv[2]
     else:
         print('Please provide input/output filename arguments')
         sys.exit(-1)
-
-    intype, num_vars, num_clauses, formula = handle_input(input_file_name)
+    num_vars, num_clauses, formula = handle_input(input_file_name)
     solution = solver_methods.dpll(formula)
-
-    print('Type: {}, number of vars {}, number of clauses {}.'.format(intype, num_vars, num_clauses))
-    print(formula)
-    print(solution)
+    print("Solution = ", solution)
--- test.py
+++ test.py
@@ -1,5 +1,10 @@
 import boolean
 
+xx = 1
+def bla(nx):
+    nx = 2
+    print(nx)
+
 x = boolean.Variable('1')
 nx = boolean.Not(x)
 
@@ -7,11 +12,14 @@ xornx = boolean.Or(boolean.T, x, nx)
 # xandnx = boolean.And(boolean.T, x, boolean.Not(boolean.Or(x, boolean.Not(x))))
 # xandnx = boolean.Not(boolean.Or(x, boolean.Not(x)))
 # xandnx = boolean.Or(x, boolean.Not(x), boolean.Variable('2'))
-xandnx = boolean.And(boolean.Not(boolean.Or(boolean.Not(x), boolean.Variable('3'))), boolean.Not(x), boolean.Variable('2'))
+# xandnx = boolean.And(boolean.Not(boolean.Or(boolean.Not(x), boolean.Variable('3'))), boolean.Not(x), boolean.Variable('2'))
 
-print(xornx)
-print(xornx.simplify())
+# print(xornx)
+# print(xornx.simplify())
 
-print(xandnx)
-print(xandnx.simplify())
+# print(xandnx)
+# print(xandnx.simplify())
 # print(xandnx.evaluate({'1':boolean.T, '2':boolean.T, '3':boolean.F}))
+
+bla(nx)
+print(nx)
