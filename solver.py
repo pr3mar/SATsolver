4eea0b06f6e60ea7596316c019b208c1c896d285
pr3mar <markoprelevik@gmail.com>
2017-10-26 19:37:02 +0200

    new version, abandoned the last one.

--- solver.py
+++ solver.py
@@ -1,50 +1,77 @@
-__author__ = 'Marko Prelevikj'
-import sys
-import boolean
-import solver_methods
+from itertools import filterfalse
+import re
 
-def handle_input(input_file_name):
-    '''
-     handles the funtion input
-    '''
-    input_type = 0
-    num_vars = 0
-    num_clauses = 0
-    formula = []
-    used_vars = dict()
-    with open(input_file_name) as file:
-        for line in file:
-            line = line.strip().split(' ')
-            if line[0] == 'c':
-                continue
-            if line[0] == 'p':
-                num_vars = int(line[2])
-                num_clauses = int(line[3])
-                continue
-            for var in line[:-1]:
-                var = abs(int(var))
-                if not(var in used_vars):
-                    used_vars[var] = boolean.Variable(var)
-            formula.append(
-                boolean.Or(
-                    [
-                        [used_vars[int(x)] 
-                        if int(x) > 0 
-                        else boolean.Not(used_vars[abs(int(x))]) 
-                        for x in line[:-1]], len(line[:-1])
-                    ]
-                )
-            )
-        formula = boolean.And([formula, len(formula)])
-        return (num_vars, num_clauses, formula)
+# parse CNF files
+def parse_inst(input_file):
+    with open(input_file) as f: s = f.read()
+    clauses = s.split("\n")
+    fs = 0
+    nvars = 0
+    parsed = []
+    vars = set()
+    for f in clauses:
+        if f.startswith("c"):
+            continue
+        elif f.startswith("p"):
+            prob = re.split("\s+", f)
+            nvars = int(prob[2])
+            fs = int(prob[3])
+        elif f.strip() == '':
+            continue
+        else:
+            cl = list(map( lambda x: int(x),  f.strip().split(" ")))
+            cl.pop()
+            vars = vars.union(set([abs(x) for x in cl]))
+            parsed.append(cl)
+    return parsed, dict.fromkeys(vars, None)
 
-if __name__ == '__main__':
-    if len(sys.argv) >= 3:
-        input_file_name = sys.argv[1]
-        output_file_name = sys.argv[2]
-    else:
-        print('Please provide input/output filename arguments')
-        sys.exit(-1)
-    num_vars, num_clauses, formula = handle_input(input_file_name)
-    solution = solver_methods.dpll(formula)
-    print("Solution = ", solution)
+def simplify(formula, valuations, add=None):
+    if not (add is None):
+        formula.insert(0, add)
+    id = 0
+    while id < len(formula): # identify all the unit clauses
+        if len(formula[id]) == 1:
+            val = formula[id][0]
+            valuations[abs(val)] = val > 0 # value needed to be true
+            iid = 0
+            while iid < len(formula): # remove all clauses where the unit literal appears
+                if val in formula[iid]:
+                    formula.remove(formula[iid])
+                    iid = 0
+                    continue
+                # formula[iid] = [e for e in formula[iid] if e not in (literalToRemove & set(formula[iid]))]
+                if -val in formula[iid]:
+                    formula[iid].remove(-val)
+                iid += 1
+            id = 0
+        else:
+            id += 1
+    return formula, valuations
+
+def SATsolver(formula, valuations, add=None):
+    formula, valuations = simplify(formula, valuations, add)
+    if formula == []: # if we managed to empty the list, the formula is satisfied
+        return valuations
+    if [] in formula: # if there is an empty clause in the formula, the formula is not satisfied
+        return None
+    for literal, value in valuations.items():
+        if value is None: # try assigning a True value to the picked literal
+            calc_valuations = SATsolver(list(formula), dict(valuations), [literal])
+            if calc_valuations is None: # try assigning a Falce value to the picked literal
+                return SATsolver(list(formula), dict(valuations), [literal])
+            else: # return the result if it exists
+                return calc_valuations 
+
+formula, vars = parse_inst('inputs/input2')
+
+solution = SATsolver(formula, vars)
+
+print(solution)
+# with open('outputs/sudoku_easy.txt', 'w') as file:
+#     if solution == None:
+#         print("FAIL")
+#         file.write('0');
+#     else:
+#         print("SUCCESS")
+#         # formula[iid] = [e for e in formula[iid] if e not in (literalToRemove & set(formula[iid]))]
+#         file.write(str([x if y else -x for x, y in solution.items()]))
