3bc5659971a1d9775feaad7815c9e3d8e9b613e5
pr3mar <markoprelevik@gmail.com>
2017-10-27 19:37:39 +0200

    updating solver

--- solver.py
+++ solver.py
@@ -1,5 +1,5 @@
-from itertools import filterfalse
-import re
+from random import shuffle
+import re, sys
 
 # parse CNF files
 def parse_inst(input_file):
@@ -19,7 +19,7 @@ def parse_inst(input_file):
         elif f.strip() == '':
             continue
         else:
-            cl = list(map( lambda x: int(x),  f.strip().split(" ")))
+            cl = list(map(lambda x: int(x), f.strip().split(" ")))
             cl.pop()
             vars = vars.union(set([abs(x) for x in cl]))
             parsed.append(cl)
@@ -48,23 +48,53 @@ def simplify(formula, valuations, add=None):
             id += 1
     return formula, valuations
 
+# def selective_simplify(formula, valuations, add):
+#     if = 0
+#     while id < len(formula): # identify all the unit clauses
+#         if len(formula[id]) == 1:
+#             val = formula[id][0]
+#             valuations[abs(val)] = val > 0 # value needed to be true
+#             iid = 0
+#             while iid < len(formula): # remove all clauses where the unit literal appears
+#                 if val in formula[iid]:
+#                     formula.remove(formula[iid])
+#                     iid = 0
+#                     continue
+#                 # formula[iid] = [e for e in formula[iid] if e not in (literalToRemove & set(formula[iid]))]
+#                 if -val in formula[iid]:
+#                     formula[iid].remove(-val)
+#                 iid += 1
+#             id = 0
+#         else:
+#             id += 1
+#     return formula, valuations
+
 def SATsolver(formula, valuations, add=None):
     formula, valuations = simplify(formula, valuations, add)
     if formula == []: # if we managed to empty the list, the formula is satisfied
         return valuations
     if [] in formula: # if there is an empty clause in the formula, the formula is not satisfied
-        return None
-    for literal, value in valuations.items():
-        if value is None: # try assigning a True value to the picked literal
-            calc_valuations = SATsolver(list(formula), dict(valuations), [literal])
-            if calc_valuations is None: # try assigning a Falce value to the picked literal
-                return SATsolver(list(formula), dict(valuations), [literal])
-            else: # return the result if it exists
-                return calc_valuations 
+        raise Exception('Contradiction!')
+    shuffled_list = [e for e, x in valuations.items() if x is None]
+    shuffle(shuffled_list)
+    for literal in shuffled_list:
+        value = valuations[literal]
+        try:
+            return SATsolver(list(formula), dict(valuations), [literal])
+        except Exception:
+            return SATsolver(list(formula), dict(valuations), [-literal])
+            # if calc_valuations is None: # try assigning a Falce value to the picked literal
+            #     return SATsolver(list(formula), dict(valuations), [-literal])
+            # else: # return the result if it exists
+            #     return calc_valuations
 
-formula, vars = parse_inst('inputs/input2')
+formula, variables = parse_inst('inputs/input2')
 
-solution = SATsolver(formula, vars)
+try:
+    solution = SATsolver(formula, variables)
+except Exception as contradiction:
+    print('FAIL:', contradiction)
+    sys.exit(0)
 
 print(solution)
 # with open('outputs/sudoku_easy.txt', 'w') as file:
