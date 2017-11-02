d91b70bc92e569ed15eae3b32cfdf09af33db2cd
lukazlatecan <luka.zlatecan@gmail.com>
2017-11-02 16:04:39 +0100

    Luka commit

--- solver.py
+++ solver.py
@@ -1,4 +1,5 @@
 from random import shuffle
+from operator import itemgetter
 import re, sys
 
 # parse CNF files
@@ -29,6 +30,7 @@ def simplify(formula, valuations, add=None):
     if not (add is None):
         formula.insert(0, add)
     id = 0
+    print(formula)
     while id < len(formula): # identify all the unit clauses
         if len(formula[id]) == 1:
             val = formula[id][0]
@@ -37,15 +39,17 @@ def simplify(formula, valuations, add=None):
             while iid < len(formula): # remove all clauses where the unit literal appears
                 if val in formula[iid]:
                     formula.remove(formula[iid])
-                    iid = 0
-                    continue
-                # formula[iid] = [e for e in formula[iid] if e not in (literalToRemove & set(formula[iid]))]
-                if -val in formula[iid]:
+                    iid -= 1
+                    print(formula)
+                elif -val in formula[iid]:
                     formula[iid].remove(-val)
+                    print(formula)
                 iid += 1
             id = 0
         else:
             id += 1
+    print(formula)
+    print " "
     return formula, valuations
 
 # def selective_simplify(formula, valuations, add):
@@ -69,35 +73,109 @@ def simplify(formula, valuations, add=None):
 #             id += 1
 #     return formula, valuations
 
-def SATsolver(formula, valuations, add=None):
+# def SATshufflerOLD(formula, valuations, add=None):
+#     formula, valuations = simplify(formula, valuations, add)
+#     if formula == []: # if we managed to empty the list, the formula is satisfied
+#         return valuations
+#     if [] in formula: # if there is an empty clause in the formula, the formula is not satisfied
+#         raise Exception('Contradiction!')
+#     shuffled_list = [e for e, x in valuations.items() if x is None]
+#     shuffle(shuffled_list)
+#     for literal in shuffled_list:
+#         try:
+#             #print "1.",literal
+#             #print valuations
+#             return SATshuffler(list(formula), dict(valuations), [literal])
+#         except Exception:
+#             #print "2.", literal
+#             #print valuations
+#             try:
+#                 return SATshuffler(list(formula), dict(valuations), [-literal])
+#             except Exception:
+#                 continue
+#     raise Exception('Contradiction2!')
+#             # if calc_valuations is None: # try assigning a Falce value to the picked literal
+#             #     return SATshuffler(list(formula), dict(valuations), [-literal])
+#             # else: # return the result if it exists
+#             #     return calc_valuations
+
+# def SATdll(formula, valuations, add=None):
+#     formula, valuations = simplify(formula, valuations, add)
+#     print(len(formula))
+#     if formula == []: # if we managed to empty the list, the formula is satisfied
+#         return valuations
+#     if [] in formula: # if there is an empty clause in the formula, the formula is not satisfied
+#         return None
+#     remain = [e for e, x in valuations.items() if x is None]
+#     for literal in remain:
+#         sol = SATdll(list(formula), dict(valuations), [literal])
+#         if sol is not None:
+#             return sol
+#         sol = SATdll(list(formula), dict(valuations), [-literal])
+#         if sol is not None:
+#             return sol
+#     return None
+
+def SATshuffler(formula, valuations, add=None):
     formula, valuations = simplify(formula, valuations, add)
     if formula == []: # if we managed to empty the list, the formula is satisfied
         return valuations
     if [] in formula: # if there is an empty clause in the formula, the formula is not satisfied
-        raise Exception('Contradiction!')
+        return None
     shuffled_list = [e for e, x in valuations.items() if x is None]
     shuffle(shuffled_list)
     for literal in shuffled_list:
-        value = valuations[literal]
-        try:
-            return SATsolver(list(formula), dict(valuations), [literal])
-        except Exception:
-            return SATsolver(list(formula), dict(valuations), [-literal])
-            # if calc_valuations is None: # try assigning a Falce value to the picked literal
-            #     return SATsolver(list(formula), dict(valuations), [-literal])
-            # else: # return the result if it exists
-            #     return calc_valuations
+        sol = SATshuffler(list(formula), dict(valuations), [literal])
+        if sol is not None:
+            return sol
+        sol = SATshuffler(list(formula), dict(valuations), [-literal])
+        if sol is not None:
+            return sol
+    print("TU")
+    return None
 
-formula, variables = parse_inst('inputs/input2')
+# def SATmaxpicker(formula, valuations, add=None):
+#     formula, valuations = simplify(formula, valuations, add)
+#     if formula == []: # if we managed to empty the list, the formula is satisfied
+#         return valuations
+#     if [] in formula: # if there is an empty clause in the formula, the formula is not satisfied
+#         raise Exception('Contradiction!')
+#     occ = dict.fromkeys((e for e, x in valuations.items() if x is None), 0)
+#     for f in formula:
+#         for v in f:
+#             occ[abs(v)] += 1
+#     try:
+#         return SATmaxpicker(list(formula), dict(valuations), [max(occ, key=occ.get)])
+#     except Exception:
+#         return SATmaxpicker(list(formula), dict(valuations), [-(max(occ, key=occ.get))])
 
-try:
-    solution = SATsolver(formula, variables)
-except Exception as contradiction:
-    print('FAIL:', contradiction)
-    sys.exit(0)
 
-print(solution)
-# with open('outputs/sudoku_easy.txt', 'w') as file:
+#Main
+# if len(sys.argv) < 3:
+#     print('Wrong input parameters!')
+#     sys.exit(0)
+
+# input_file = sys.argv[1]
+# output_file = sys.argv[2]
+# formula, variables = parse_inst(input_file)
+formula, variables = parse_inst('inputs/input5')
+
+#Try SATshuffler
+solution = SATshuffler(list(formula), dict(variables))
+if solution is not None:
+    print(solution)
+else:
+    print("FAIL")
+
+#Try SATmaxpicker
+# try:
+#     solution = SATmaxpicker(formula, variables)
+# except Exception as contradiction:
+#     print('FAIL:', contradiction)
+#     sys.exit(0)
+# print(solution)
+
+# with open(output_file, 'w') as file:
 #     if solution == None:
 #         print("FAIL")
 #         file.write('0');
